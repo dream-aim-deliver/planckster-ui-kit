787dd503294bf73476bed82e8cdb54d543aa84ae
harsh010102 <hrshprkh112@gmail.com>
2024-02-06 11:33:26 +0530

    issue 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,11 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: "#FFA500",
-      }
+        primary: "#FFA500", // Orange
+        secondary: "#4CAF50", // Green
+        tertiary: "#3498db", // Blue
+        neutral: "#808080", // Gray
+      },
     },
   },
   plugins: [],
