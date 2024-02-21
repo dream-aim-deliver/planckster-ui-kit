ebaf951d439f22c5e1687641d974e8b180c544f8
iamMihirT <mihirt1606@gmail.com>
2024-02-21 14:56:37 +0530

    filter and sort feature added

---
+++ lib/components/filter/filter.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useFilterStore } from './filter.logic';
+
+interface FilterProps {
+  onApplyFilter: (startDate: Date, endDate: Date) => void;
+}
+
+const Filter: React.FC<FilterProps> = ({ onApplyFilter }) => {
+  const { startDate, endDate, setStartDate, setEndDate, applyFilter } = useFilterStore();
+
+  return (
+    <div>
+      <label htmlFor="startDate">Start Date:</label>
+      <input
+        type="date"
+        id="startDate"
+        value={startDate ? startDate.toISOString().split('T')[0] : ''}
+        onChange={(e) => setStartDate(new Date(e.target.value))}
+      />
+      <label htmlFor="endDate">End Date:</label>
+      <input
+        type="date"
+        id="endDate"
+        value={endDate ? endDate.toISOString().split('T')[0] : ''}
+        onChange={(e) => setEndDate(new Date(e.target.value))}
+      />
+      <button onClick={applyFilter}>Apply Filter</button>
+    </div>
+  );
+};
+
+export default Filter;
